152ed811ffec34043b4243c0fcf36b32764047a3
marlondck <15050029+marlondck@users.noreply.github.com>
2019-08-15 19:12:21 -0400

    Hash de senhas bcrypt

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,17 +1,30 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
     super.init(
       {
+        // -> Os campos abaixo representao quais receberao dados ou alteracoes
+        // -> O Campo password é virtual e nao esta presente no model de User
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+    // -> Metodo antes de salvar
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        // -> Caso existir um password user o bcript vai usar essa string e fazer um hash de senha
+        // -> O Nº 8 é a sua força
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+    return this;
   }
 }
 export default User;
