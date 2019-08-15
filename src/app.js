507aca2519779b0a070078032cbcc5d86c88a6fb
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-08-15 19:02:16 -0400

    Loader para os Models

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import routes from './routes';
+// -> Chama um loader de models
+import './database';
 
 class App {
   constructor() {
---
+++ src/database/index.js
@@ -0,0 +1,24 @@
+import Sequelize from 'sequelize';
+import User from '../app/models/User';
+import databaseConfig from '../config/database';
+
+// Vetor de models que sera repassada no metodo init mais abaixo
+const models = [User];
+
+class Database {
+  // -> Método que inicia automaticamente,
+  //   as funcoes repassadas para ele serao automaticamente executadas
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    // -> Inicia uma conexao com o DB
+    this.connection = new Sequelize(databaseConfig);
+    // -> faz um mapeamento de todos os models
+    // -> dentro de cada model é chamado a funcao init e repassado a conexao do DB
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
