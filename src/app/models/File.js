e4db25b8c8bfacff378eb2092a7eb9a3c2d405c2
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-08-19 18:43:59 -0400

    Acesso a caminho de arquivos

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -6,6 +6,13 @@ class File extends Model {
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        // -> Este campo virtual irá retornar uma url para ser usada no frontend
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
