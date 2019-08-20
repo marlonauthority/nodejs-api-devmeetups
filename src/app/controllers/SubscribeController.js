97ed446c6adf1dd15498bd8cd4849725f10eadb1
Marlon Authority <marlon@MacBook-Pro-de-Marlon.local>
2019-08-20 18:55:47 -0400

    nao pode inscrever no mesmo horario

--- src/app/controllers/SubscribeController.js
+++ src/app/controllers/SubscribeController.js
@@ -5,6 +5,10 @@ import Meetup from '../models/Meetup';
 class SubscribeController {
   async store(req, res) {
     const meetup = await Meetup.findByPk(req.params.meetupId);
+    // -> Se nao encontrar 404..
+    if (!meetup) {
+      return res.status(404).json({ error: 'Meetup não existe.' });
+    }
     // return res.json(meetup);
     // -> se nao for um owner
     if (meetup.user_id === req.userId) {
@@ -35,8 +39,35 @@ class SubscribeController {
         .status(401)
         .json({ erro: 'Não é possível se inscrever mais de uma vez.' });
     }
-    // return res.json(isSubscribed);
-    // return res.json(meetupId);
+    //
+    // -> Agendamento no mesmo horario?
+    const checkAvailability = await Subscribers.findOne({
+      // onde o user_id seja o mesmo que faz a requisicao
+      where: {
+        user_id: req.userId,
+      },
+      // inclua o relacionamento com meetup
+      include: [
+        {
+          model: Meetup,
+          as: 'meetup',
+          required: true,
+          // onde a data e hora seja a mesma a qual o user esta tentando subscrever
+          where: {
+            date_hour: meetup.date_hour,
+          },
+        },
+      ],
+    });
+    // return res.json(checkAvailability);
+    // -> se ele encontrou a inscricao significa que ja se inscreveu em um meetup do mesmo horario..
+    if (checkAvailability) {
+      return res.status(400).json({
+        error: 'Você já possuí inscrição em um Meetup do mesmo horario.',
+      });
+    }
+    //
+    // Se passou por todas validacoes..
     const subscription = await Subscribers.create({
       meetup_id: req.params.meetupId,
       user_id: req.userId,
