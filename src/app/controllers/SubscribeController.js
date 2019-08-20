785d6d798623f28577d467c40b1bff63b8c4cdb3
marlondck <15050029+marlondck@users.noreply.github.com>
2019-08-20 17:24:25 -0400

    nao e possivel se increver em meetups antigos

--- src/app/controllers/SubscribeController.js
+++ src/app/controllers/SubscribeController.js
@@ -1,3 +1,4 @@
+import { isBefore } from 'date-fns';
 import Subscribers from '../models/Subscribers';
 import Meetup from '../models/Meetup';
 
@@ -5,12 +6,21 @@ class SubscribeController {
   async store(req, res) {
     const meetup = await Meetup.findByPk(req.params.meetupId);
     // return res.json(meetup);
-    // -> se nao for um prestador
+    // -> se nao for um owner
     if (meetup.user_id === req.userId) {
       return res.status(401).json({
         erro:
           'Só é possível se inscrever em Meetups ao qual você não seja o dono.',
       });
+    }
+    // -> Checagem das datas
+    // const hoje = new Date();
+    // const dataDB = meetup.date_hour;
+    // const before = isBefore(dataDB, hoje);
+    // return res.json({ hoje, dataDB, before });
+    // -> Checa no banco de dados
+    if (isBefore(meetup.date_hour, new Date())) {
+      return res.status(400).json({ error: 'Este Meetup já passou.' });
     }
     // return res.json(meetupId);
     const subscription = await Subscribers.create({
