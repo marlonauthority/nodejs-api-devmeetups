1dd900b55bddbb57bec7502b16a73d704af05e19
marlondck <15050029+marlondck@users.noreply.github.com>
2019-08-20 17:19:14 -0400

    subscribe em meetup != do owner

--- src/app/controllers/SubscribeController.js
+++ src/app/controllers/SubscribeController.js
@@ -3,10 +3,18 @@ import Meetup from '../models/Meetup';
 
 class SubscribeController {
   async store(req, res) {
-    const { meetupId } = req.params;
+    const meetup = await Meetup.findByPk(req.params.meetupId);
+    // return res.json(meetup);
+    // -> se nao for um prestador
+    if (meetup.user_id === req.userId) {
+      return res.status(401).json({
+        erro:
+          'Só é possível se inscrever em Meetups ao qual você não seja o dono.',
+      });
+    }
     // return res.json(meetupId);
     const subscription = await Subscribers.create({
-      meetup_id: meetupId,
+      meetup_id: req.params.meetupId,
       user_id: req.userId,
     });
     return res.json(subscription);
